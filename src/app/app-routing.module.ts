965cbe4a5d93e30949a77f10573de66a3b0e79e7
softline <kraszka1234@gmail.com>
2020-09-04 14:26:57 +0200

    again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { RepertuarComponent } from './repertuar/repertuar.component';
 import { PriceListComponent } from './price-list/price-list.component';
 import { ActorComponent } from './actor/actor.component';
 const routes: Routes = [
-  {path: '/repertuar', component: RepertuarComponent},
-  {path: '/priceList', component: PriceListComponent},
-  {path: '/actors', component: ActorComponent}
-]
+  {path: 'repertuar', component: RepertuarComponent},
+  {path: 'priceList', component: PriceListComponent},
+  {path: 'actors', component: ActorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
