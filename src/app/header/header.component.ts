6e564a5ef782ec29fec00ebb160641d2842eb431
softline <kraszka1234@gmail.com>
2020-09-08 11:17:34 +0200

    added responsive navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  status = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(): void{
+  this.status = !this.status;
 }
+}
+ 
