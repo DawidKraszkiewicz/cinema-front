b762864feacffae985d43f86cae193fda6cad4c5
softline <kraszka1234@gmail.com>
2020-09-09 13:04:29 +0200

    fix

--- src/app/repertuar/shared/repertuar.service.ts
+++ src/app/repertuar/shared/repertuar.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { baseUrl } from '../../../environments/config';
 
-const baseUrl = 'http://localhost:3000/api/film';
+
 
 @Injectable({
   providedIn: 'root'
